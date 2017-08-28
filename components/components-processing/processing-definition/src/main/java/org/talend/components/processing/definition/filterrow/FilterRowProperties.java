48d804ae34ff0036ba4241e5749d454e1c8d90a4
BOURGEOIS Laurent <lbourgeois@talend.com>
2017-08-28 15:32:52 +0200

    feat(TFD-1434) : Add widget type to logical op

--- components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/filterrow/FilterRowProperties.java
+++ components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/filterrow/FilterRowProperties.java
@@ -84,6 +84,7 @@ public class FilterRowProperties extends FixedConnectorsComponentProperties {
     public void setupLayout() {
         super.setupLayout();
         Form mainForm = new Form(this, Form.MAIN);
+        mainForm.addRow(Widget.widget(logicalOp).setWidgetType(Widget.ENUMERATION_WIDGET_TYPE));
         mainForm.addRow(Widget.widget(filters).setWidgetType(Widget.NESTED_PROPERTIES)
                 .setConfigurationValue(Widget.NESTED_PROPERTIES_TYPE_OPTION, "filter"));
     }
