5368e8bdba983c5acbc1fd46712586a6f2d5925b
undx <emmanuel.gallois@gmail.com>
2017-09-14 18:44:05 +0200

    feat(TDI-39454): Add Copyright notice and update some comp titles

--- components/components-google-drive/components-google-drive-definition/src/main/java/org/talend/components/google/drive/GoogleDriveFamilyDefinition.java
+++ components/components-google-drive/components-google-drive-definition/src/main/java/org/talend/components/google/drive/GoogleDriveFamilyDefinition.java
@@ -1,4 +1,18 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.components.google.drive;
+
+import aQute.bnd.annotation.component.Component;
 
 import org.talend.components.api.AbstractComponentFamilyDefinition;
 import org.talend.components.api.ComponentInstaller;
@@ -10,8 +24,6 @@ import org.talend.components.google.drive.delete.GoogleDriveDeleteDefinition;
 import org.talend.components.google.drive.get.GoogleDriveGetDefinition;
 import org.talend.components.google.drive.list.GoogleDriveListDefinition;
 import org.talend.components.google.drive.put.GoogleDrivePutDefinition;
-
-import aQute.bnd.annotation.component.Component;
 
 @Component(name = Constants.COMPONENT_INSTALLER_PREFIX + GoogleDriveFamilyDefinition.NAME, provide = ComponentInstaller.class)
 public class GoogleDriveFamilyDefinition extends AbstractComponentFamilyDefinition implements ComponentInstaller {
