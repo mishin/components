a7d4980e3aefa0958bb99c932bd48fa084a0d371
Dmytro Sylaiev <dsylaiev@talend.com>
2017-09-21 10:35:29 +0300

    feat(TDI-39467): Implement basic bulkload functions

--- components/components-marklogic/components-marklogic-runtime/src/main/java/org/talend/components/marklogic/runtime/MarkLogicBulkLoad.java
+++ components/components-marklogic/components-marklogic-runtime/src/main/java/org/talend/components/marklogic/runtime/MarkLogicBulkLoad.java
@@ -12,20 +12,55 @@
 // ============================================================================
 package org.talend.components.marklogic.runtime;
 
+import com.marklogic.contentpump.ContentPump;
 import org.talend.components.api.component.runtime.ComponentDriverInitialization;
 import org.talend.components.api.container.RuntimeContainer;
+import org.talend.components.marklogic.tmarklogicbulkload.MarkLogicBulkLoadProperties;
+import org.talend.daikon.i18n.GlobalI18N;
+import org.talend.daikon.i18n.I18nMessages;
 import org.talend.daikon.properties.Properties;
 import org.talend.daikon.properties.ValidationResult;
+import org.talend.daikon.properties.ValidationResultMutable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class MarkLogicBulkLoad implements ComponentDriverInitialization {
+    private static final I18nMessages i18nMessages = GlobalI18N.getI18nMessageProvider()
+            .getI18nMessages(MarkLogicBulkLoad.class);
 
+    private MarkLogicBulkLoadProperties bulkLoadProperties;
     @Override
     public void runAtDriver(RuntimeContainer container) {
-
+        try {
+            ContentPump.runCommand(new String[] {"import",
+                    "-username", bulkLoadProperties.connection.username.getStringValue(),
+                    "-password", bulkLoadProperties.connection.password.getStringValue(),
+                    "-host", bulkLoadProperties.connection.host.getStringValue(),
+                    "-port", String.valueOf(bulkLoadProperties.connection.port.getValue()),
+                    "-database", bulkLoadProperties.connection.database.getStringValue(),
+                    "-output_uri_replace", "\"D:/data/bulk_test/,'" + bulkLoadProperties.docidPrefix.getStringValue() + "'\"",
+                    "-input_file_path", bulkLoadProperties.loadFolder.getStringValue()});
+        } catch (Exception e) {
+            e.printStackTrace();
+            //TODO do here smth
+        }
     }
 
     @Override
     public ValidationResult initialize(RuntimeContainer container, Properties properties) {
-        return ValidationResult.OK;
+        ValidationResultMutable validationResult = new ValidationResultMutable();
+        if (properties instanceof MarkLogicBulkLoadProperties) {
+            bulkLoadProperties = (MarkLogicBulkLoadProperties) properties;
+            validationResult.setStatus(ValidationResult.Result.OK);
+        } else {
+            validationResult.setStatus(ValidationResult.Result.ERROR);
+            validationResult.setMessage(i18nMessages.getMessage("error.wrongProperties"));
+
+        }
+        return validationResult;
     }
 }
---
+++ components/components-marklogic/components-marklogic-runtime/src/test/java/org/talend/components/marklogic/runtime/MarkLogicBulkLoadTest.java
@@ -0,0 +1,14 @@
+package org.talend.components.marklogic.runtime;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MarkLogicBulkLoadTest {
+
+    @Test
+    @Ignore
+    public void mainTest() {
+        new MarkLogicBulkLoad().runAtDriver(null);
+    }
+
+}
